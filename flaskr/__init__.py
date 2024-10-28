0cf29ba2a11a1d3b97374a568b331ef8cc6f150f
hmng <hmng@node-0.hmng-225596.ulambda-pg0.clemson.cloudlab.us>
2024-10-28 14:29:19 -0400

    a setup, not a good one but there you go

---
+++ flaskr/__init__.py
@@ -0,0 +1,131 @@
+import os
+import ctypes
+import functools
+from resource import *
+import time
+
+from flask import Flask
+from flask import request
+
+
+# setup to make syscalls
+
+libc = ctypes.CDLL(None, use_errno=True)
+
+class sched_attr(ctypes.Structure):
+    _fields_ = [
+        ('size', ctypes.c_uint32),
+        ('sched_policy', ctypes.c_uint32),
+        ('sched_flags', ctypes.c_uint64),
+        ('sched_nice', ctypes.c_int32),
+        ('sched_priority', ctypes.c_uint32),
+        ('sched_runtime', ctypes.c_uint64),
+        ('sched_deadline', ctypes.c_uint64),
+        ('sched_period', ctypes.c_uint64),
+    ]
+
+def sched_getattr(pid, attr):
+    attr.size = ctypes.sizeof(attr)
+    ret = libc.syscall(315, pid, ctypes.byref(attr), attr.size, 0)
+    if ret == -1:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno))
+    return attr
+
+
+def sched_setattr(pid, attr):
+    attr.size = ctypes.sizeof(attr)
+    ret = libc.syscall(314, pid, ctypes.byref(attr), 0)
+    if ret == -1:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno))
+
+
+# define decorator
+
+def add_deadline(deadline):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            # can use this to check against method type if we want to selectively apply the deadline 
+            # (eg diff deadline for GET vs POST, via passing a dict or via having multiple decorators)
+            # print(request.method)
+
+            ns_per_ms = 1000000
+            attr = sched_attr()
+            attr = sched_getattr(0, attr)
+
+            attr.sched_runtime = deadline * ns_per_ms
+            sched_setattr(0, attr)
+
+            start_time = time.time() * 1000
+
+            # Call the original function
+            result = func(*args, **kwargs)
+
+            attr.sched_runtime = 4 * ns_per_ms
+            sched_setattr(0, attr)
+
+            end_time = time.time() * 1000
+            time_passed = end_time - start_time
+
+            # also print out latency results
+            usage_stats = getrusage(RUSAGE_SELF)
+
+            # stats are given in seconds, convert to ms
+            runtime = usage_stats.ru_utime * 1000 + usage_stats.ru_stime * 1000
+            f = open("latency.txt", "a")
+            f.write(str(end_time) + " - latency: time passed: (inside: " + str(time_passed) + ", outside: " + str(time_passed) + "), deadline: " + str(deadline) + ", rusage: " + str(runtime) + "\n")
+            f.close()
+
+            return result
+        return wrapper
+    return decorator
+
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # register the database commands
+    from . import db
+
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from . import blog
+    from . import auth
+
+    app.register_blueprint(blog.bp)
+    app.register_blueprint(auth.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
+
+    open("latency.txt", 'w').close()
+
+    return app
---
+++ flaskr/blog.py
@@ -0,0 +1,138 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
+import os
+
+from .db import get_db
+from . import add_deadline
+
+bp = Blueprint("blog", __name__)
+
+UPLOAD_FOLDER = '/users/hmng/tutorial/uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+
+
+@bp.route("/")
+@add_deadline(16)
+def index():
+    """Show all the posts, most recent first."""
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
+
+@bp.route("/<int:id>")
+@add_deadline(8)
+def get_post(id):
+    """Show a single post."""
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    
+    print(post['title'])
+    print(post['created'])
+    
+    return render_template("blog/index.html", posts=[post])
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@add_deadline(12)
+def create():
+    """Create a new post for the current user."""
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def count_words(filename):
+    text = open(filename, "r") 
+    d = dict() 
+    
+    for line in text: 
+        line = line.strip()  
+        line = line.lower() 
+        words = line.split(" ") 
+
+        for word in words: 
+            if word in d: 
+                d[word] = d[word] + 1
+            else: 
+                d[word] = 1
+    return d
+
+@bp.route('/docount', methods=['GET', 'POST'])
+@add_deadline(30)
+def do_count():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            
+            # do some processing of the file
+            word_dict = count_words(os.path.join(UPLOAD_FOLDER, filename))
+            s = dict(sorted(word_dict.items(), key=lambda item: item[1]))
+            l = [(k, v) for (k, v) in s.items()]
+            popular_word, file_count = l[0]
+
+            os.remove(os.path.join(UPLOAD_FOLDER, filename))
+
+            return render_template("blog/success.html", word=popular_word, count=file_count)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
